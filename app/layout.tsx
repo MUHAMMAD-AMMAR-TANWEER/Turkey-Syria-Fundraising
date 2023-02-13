08f3285ee671de7c64a38bbac5884362efc44c3e
MUHAMMAD-AMMAR-TANWEER <ammartanweer7370@gmail.com>
2023-02-13 02:45:12 -0800

    Let's Start Building

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+"use client"
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function RootLayout({
   children,
@@ -12,7 +13,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <NextUIProvider>
+        {children}
+        </NextUIProvider>
+      </body>
     </html>
   )
 }
